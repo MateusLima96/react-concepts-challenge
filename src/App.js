7b19456da8a68e835e7a2ddfd318b65ce0dc781f
Mateus Lima <mateus.lima@osf.digital>
2020-05-16 14:12:18 -0300

    adds  list, add, remove repository

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      title: `Desafio Mongo DB ${Date.now()}`, 
+      url: "http://github.com/nodejs", 
+      techs: ["React.js","JS"],
+      likes: 0
+    });
+    
+    const repository = response.data;
+    
+    setRepositories([...repositories, repository]);
   }
 
+
+
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    //retorna um novo array com todos elementos do array que sao diferentes do elemento passado como parametro
+    const deletedRepository = repositories.filter(
+      repository => repository.id !== id
+    );
+
+
+    setRepositories([...deletedRepository]);
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository =><li key = {repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+          </button> 
+        </li>)}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+          <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
